cab81769a369790815e778163fef9f384905a1cc
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-05-03 16:49:41 +0200

    test datashader

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,15 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+import datetime
+import xarray as xr
+import datashader as ds
+import datashader.transfer_functions as tf
+from collections import OrderedDict
+
+from holoviews.operation.datashader import datashade
+from holoviews.streams import PlotSize
+
 import hvplot
 import hvplot.pandas
 
@@ -11,7 +20,44 @@ st.title('🎈 App Name')
 
 st.write('Hello world!')
 
-hv.extension('bokeh', logo=False)
+# Constants
+np.random.seed(2)
+n = 100000                               # Number of points
+cols = list('abcdefg')                   # Column names of samples
+start = datetime.datetime(2010, 10, 1, 0)   # Start time
+
+# Generate a fake signal
+signal = np.random.normal(0, 0.3, size=n).cumsum() + 50
+
+# Generate many noisy samples from the signal
+noise = lambda var, bias, n: np.random.normal(bias, var, n)
+data = {c: signal + noise(1, 10*(np.random.random() - 0.5), n) for c in cols}
+
+# Add some "rogue lines" that differ from the rest 
+cols += ['x'] ; data['x'] = signal + np.random.normal(0, 0.02, size=n).cumsum() # Gradually diverges
+cols += ['y'] ; data['y'] = signal + noise(1, 20*(np.random.random() - 0.5), n) # Much noisier
+cols += ['z'] ; data['z'] = signal # No noise at all
+
+# Pick a few samples from the first line and really blow them out
+locs = np.random.choice(n, 10)
+data['a'][locs] *= 2
+
+# Create a dataframe
+data['Time'] = [start + datetime.timedelta(minutes=1)*i for i in range(n)]
+
+df = pd.DataFrame(data)
+df['ITime'] = pd.to_datetime(df['Time']).astype('int64')
+# Default plot ranges:
+x_range = (df.iloc[0].ITime, df.iloc[-1].ITime)
+y_range = (1.2*signal.min(), 1.2*signal.max())
+
+PlotSize.scale=2
+hv.extension('bokeh')
+
+opts = hv.opts.RGB(width=600, height=300)
+ndoverlay = hv.NdOverlay({c:hv.Curve((df['Time'], df[c]), kdims=['Time'], vdims=['Value']) for c in cols})
+st.write(hv.render(datashade(ndoverlay, cnorm='linear', aggregator=ds.count(), line_width=2).opts(opts), backend='bokeh'))
+
 
 # create sample data
 @st.cache
